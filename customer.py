574e5ea60162bd4e2f30888cd80c14cffaaff6e6
unknown <rajarshidutta18@gmail.com>
2025-10-20 20:00:09 +0530

    customer table update and dynamic allocation created

--- customer.py
+++ customer.py
@@ -1,13 +1,18 @@
-import sqlite3
+import mysql.connector
 
-conn = sqlite3.connect('CUSTOMER_MANAGEMENT')
+conn = mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="6000",
+  database = "customer_management"
+)
 cursor = conn.cursor()
 
 cname = input("Enter the customer name: ")
 cmobile = int(input("Enter the mobile number: "))
 
-# ✅ Use placeholders for variables
-cursor.execute("INSERT INTO CUSTOMER (gname, gmobile) VALUES (?, ?)", (cname, cmobile))
+sql = "INSERT INTO CUSTOMER (gname, gmobile) VALUES (%s,%s)"
+cursor.execute(sql, (cname, cmobile))
 
 conn.commit()
 print("Customer added successfully!")
--- db1.sql
+++ db1.sql
@@ -2,5 +2,5 @@ CREATE DATABASE CUSTOMER_MANAGEMENT;
 CREATE TABLE CUSTOMER(
     gid int auto_increment primary key,
     gname varchar(100),
-    gmobile int
+    gmobile VARCHAR(10)
 );
